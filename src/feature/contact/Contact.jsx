c5a6bbb2c09937b710326594836edf4d6c069c73
GigaPok <gigapokera@gmail.com>
2024-03-03 19:13:09 +0400

    change location

--- src/feature/contact/Contact.jsx
+++ src/feature/contact/Contact.jsx
@@ -22,7 +22,7 @@ export const Contact = () => {
               loading="lazy"
               allowfullscreen
               referrerpolicy="no-referrer-when-downgrade"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d91160.57954777339!2d26.012237252570365!3d44.43791870165346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1f93abf3cad4f%3A0xac0632e37c9ca628!2sBucharest%2C%20Romania!5e0!3m2!1sen!2sge!4v1709025932966!5m2!1sen!2sge"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d152515.36595460802!2d-6.410508560536985!3d53.32441163081538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e80ea27ac2f%3A0xa00c7a9973171a0!2sDublin%2C%20Ireland!5e0!3m2!1sen!2sge!4v1709478731872!5m2!1sen!2sge"
             ></iframe>
           </div>
           <div>
