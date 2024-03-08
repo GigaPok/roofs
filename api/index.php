e4b370648c135d1a3c07141c8359bb62df33d9cb
GigaPok <gigapokera@gmail.com>
2024-03-09 00:11:59 +0400

    add backend php

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+// header('Access-Control-Allow-Origin: *');
+
+$data = json_decode(file_get_contents('php://input'), true);
+$fullname = $data['fullname'];
+$eircode = $data['eircode'];
+$phone = $data['phone'];
+$message = $data['message'];
+$email = $data['email'];
+$message = 'Full name: ' . $fullname . "\r\n" . 'Eircode: ' . $eircode . "\r\n" . 'Phone: ' . $phone . "\r\n" . 'Email: ' . $email . "\r\n" . 'Message: ' . $message;
+
+$recipient = 'thekmconstruction@gmail.com';
+$subject = 'New message from ' . $fullname;
+$headers = 'From:'.$fullname.'<'.$email.'>';
+
+$send = mail($recipient, $subject, $message, $headers) or die("Error!");
+
+exit;
